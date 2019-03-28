c483043370e257c57dd71c9635e304f78b4de0bc
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-03-27 23:08:27 -0400

    Update slope1.py

--- slope1.py
+++ slope1.py
@@ -12,39 +12,38 @@
 #   y2: y coordinate of point 2
 #
 # Outputs:
-#   m: slope of the line
-#
-# User enters the coordinates of 2 points (x1, y1) and (x2, y2)
+#   m: slope of the line    
+
 # Verify the coordinates are numeric. 
-inputError = False
+def validate_input(x1,y1,x2,y2):
+    if not x1.isnumeric(): 
+        print ("Error: Enter a number for x1")
+        return False
+        
+    if not y1.isnumeric(): 
+        print ("Error: Enter a number for y1")
+        return False
+    
+    if not x2.isnumeric(): 
+        print ("Error: Enter a number for x2")
+        return False
+    
+    if not y2.isnumeric(): 
+        print ("Error: Enter a number for y2")    
+        return False
+
+    return True
+
+# main
+# User enters the coordinates of 2 points (x1, y1) and (x2, y2)
 x1=input("Enter x coordinate of point 1: ")
 y1=input("Enter y coordinate of point 1: ")
 x2=input("Enter x coordinate of point 2: ")
 y2=input("Enter y coordinate of point 2: ")
 
-if x1.isnumeric(): 
-    x1=int(x1)
-else:
-    print ("Error: Enter a number for x1")
-    inputError = True
-    
-if y1.isnumeric(): 
-    y1=int(y1)
-else:
-    print ("Error: Enter a number for y1")
-    inputError = True
-
-if x2.isnumeric(): 
-    x2=int(x2)
-else:
-    print ("Error: Enter a number for x2")
-    inputError = True
-
-if y2.isnumeric(): 
-    y2=int(y2)
-else:
-    print ("Error: Enter a number for y2")    
-    inputError = True
-
 # Calculate the slope 
-
+if validate_input(x1,y1,x2,y2):
+    x1=int(x1)
+    y1=int(y1)
+    x2=int(x2)
+    y2=int(y2)
