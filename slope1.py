7fa267125b8500540e42f6838b61e40b7d2b4982
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-02 05:57:51 -0400

    Update slope1.py

--- slope1.py
+++ slope1.py
@@ -19,45 +19,14 @@
 #   m: slope of the line    
 
 # Verify the coordinates are numeric. 
-def validate_input(x1,y1,x2,y2):
-    
+def isvalid(x1,y1,x2,y2):
     try:
         x1 = float(x1)
-    except ValueError:
-        return False
-    
-    try:
         y1 = float(y1)
-    except ValueError:
-        return False
-    
-    try:
         x2 = float(x2)
-    except ValueError:
-        return False
-    
-    try:
         y2 = float(y2)
     except ValueError:
         return False
-    
-    '''
-    if not x1.isnumeric(): 
-        print ("Error: Enter a number for x1")
-        return False
-        
-    if not y1.isnumeric(): 
-        print ("Error: Enter a number for y1")
-        return False
-    
-    if not x2.isnumeric(): 
-        print ("Error: Enter a number for x2")
-        return False
-    
-    if not y2.isnumeric(): 
-        print ("Error: Enter a number for y2")    
-        return False
-    '''
     
     return True
 
@@ -68,7 +37,7 @@ ix2=input("Enter x coordinate of point 2: ")
 iy2=input("Enter y coordinate of point 2: ")
 
 # Calculate the slope 
-if validate_input(ix1,iy1,ix2,iy2):
+if isvalid(ix1,iy1,ix2,iy2):
     ix1=float(ix1)
     iy1=float(iy1)
     ix2=float(ix2)
