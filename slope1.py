869c847c4688a382175030155b6e489a6a5b4289
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-04 08:42:51 -0400

    Update slope1.py

--- slope1.py
+++ slope1.py
@@ -9,7 +9,7 @@
 #  2. Find the equation of the line in slope intercept form
 #  3. Find the equation of the line in point slope form
 #
-def main(x1, y1, x2, y2) :
+def main() :
     run = x2-x1
     rise = y2-y1 
     if run == 0:
@@ -22,17 +22,19 @@ def main(x1, y1, x2, y2) :
     
 ## 
 #  Vertical Line
+#  a is the x-intercept
 #
-def verticalLine(x1) :
+def verticalLine(a) :
     print ("slope is undefined")
-    print ("x = ", x1)
+    print ("x = ", a)
     
 ## 
 #  Horizontal Line
+#  b is the y-intercept
 #
-def horizontalLine(y1) :
+def horizontalLine(b) :
     print ("slope = 0")
-    print ("y = ", y1)    
+    print ("y = ", b)    
     
 ## 
 #  Diagonal Line
@@ -47,26 +49,26 @@ def diagonalLine(x1, y1, m) :
 # Testcase #1 Diagonal Line Positive Slope
 print()
 print("Testcase #1 Diagonal Line")
-ix1 = -2
-iy1 = 0 
-ix2 = 0
-iy2 = 4
-main(ix1, iy1, ix2, iy2)
+x1 = -2
+y1 = 0 
+x2 = 0
+y2 = 4
+main()
 
 # Testcase #2 Vertical Line
 print()
 print("Testcase #2 Vertical Line")
-ix1 = -2
-iy1 = 0 
-ix2 = -2
-iy2 = 4
-main(ix1, iy1, ix2, iy2)
+x1 = -2
+y1 = 0 
+x2 = -2
+y2 = 4
+main()
 
 # Testcase #3 Horizontal Line
 print()
 print("Testcase #3 Horizontal Line")
-ix1 = -8
-iy1 = 3 
-ix2 = 2
-iy2 = 3
-main(ix1, iy1, ix2, iy2)
+x1 = -8
+y1 = 3 
+x2 = 2
+y2 = 3
+main()
