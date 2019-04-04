ee62ddf3d6b9cef160e72f4827c322eb6bcc6302
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-03 23:56:03 -0400

    Update slope1.py

--- slope1.py
+++ slope1.py
@@ -16,7 +16,7 @@ def main(x1, y1, x2, y2) :
         verticalLine(x1)
     elif rise == 0:
         horizontalLine(y1)
-    else
+    else:
         diagonalLine(x1, y1, rise/run)
          
     
@@ -41,13 +41,32 @@ def diagonalLine(x1, y1, m) :
     print ("slope = ", m)
     yIntercept = y1 - (m * x1)
     print ("y = "+str(m)+"x"+" + "+str(yIntercept)) 
-    print ("(y - "+str(y1)+") = str(m)+"(x - " + str(x1) + ")")
+    print ("(y - "+str(y1)+") = "+str(m)+"(x - " + str(x1) + ")")
     
 # Start the program.
-# Testcase #1
-ix1 = 0
+# Testcase #1 Diagonal Line Positive Slope
+print()
+print("Testcase #1 Diagonal Line")
+ix1 = -2
 iy1 = 0 
-ix2 = 5
-iy2 = 5
+ix2 = 0
+iy2 = 4
 main(ix1, iy1, ix2, iy2)
 
+# Testcase #2 Vertical Line
+print()
+print("Testcase #2 Vertical Line")
+ix1 = -2
+iy1 = 0 
+ix2 = -2
+iy2 = 4
+main(ix1, iy1, ix2, iy2)
+
+# Testcase #3 Horizontal Line
+print()
+print("Testcase #3 Horizontal Line")
+ix1 = -8
+iy1 = 3 
+ix2 = 2
+iy2 = 3
+main(ix1, iy1, ix2, iy2)
