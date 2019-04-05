46c4634729ffafde5c5ee38f761d292ce67c0e35
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-05 10:52:16 -0400

    Create num_days_in_month.py

---
+++ num_days_in_month.py
@@ -0,0 +1,14 @@
+##
+#  num_days_in_month.py
+#  Author: Mrs. Goldberg
+#  Date: 4/5/2019
+#
+#  Create a program that reads the name of a month. Then your program should display the number of days in that month. 
+#  Display “28 or 29 days” for February so that leap years are addressed.
+
+def main():
+  day = input("Enter the name of a month: ") 
+    
+    
+# Start the program.
+main()
