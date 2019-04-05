819104b10c00b64a2f2921ecdbba874583a3d2bc
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-05 10:56:48 -0400

    Create name_triangle.py

---
+++ name_triangle.py
@@ -0,0 +1,18 @@
+##
+#  name_triangle.py
+#  Author: Mrs. Goldberg
+#  Date: 4/5/2019
+#
+#  A triangle can be classified based on the lengths of its sides as equilateral, isosceles or scalene. 
+#  All 3 sides of an equilateral triangle have the same length. 
+#  An isosceles triangle has two sides that are the same length, and a third side that is a different length. 
+#  If all of the sides have different lengths then the triangle is scalene. 
+#  Write a program that reads the lengths of 3 sides of a triangle. Display a message indicating the type of the triangle.
+def main():
+  a = input("Enter the length of side 1: ") 
+  b = input("Enter the length of side 2: ") 
+  c = input("Enter the length of side 3: ") 
+    
+    
+# Start the program.
+main()
