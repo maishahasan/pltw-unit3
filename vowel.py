b9a63bd254e1c294d00d93e723ee6839e648ac3b
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-05 10:44:10 -0400

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,16 +1,17 @@
-Vowel or Consonant
-User Story
-In this exercise you will create a program that reads a letter of the alphabet from the
-user. If the user enters a, e, i, o or u then your program should display a message
-indicating that the entered letter is a vowel. If the user enters y then your program
-should display a message indicating that sometimes y is a vowel, and sometimes y is
-a consonant. Otherwise your program should display a message indicating that the
-letter is a consonant.
-Vowel or Consonant
-User Story
-In this exercise you will create a program that reads a letter of the alphabet from the
-user. If the user enters a, e, i, o or u then your program should display a message
-indicating that the entered letter is a vowel. If the user enters y then your program
-should display a message indicating that sometimes y is a vowel, and sometimes y is
-a consonant. Otherwise your program should display a message indicating that the
-letter is a consonant.
+##
+#  vowel.py
+#  Author: Mrs. Goldberg
+#  Date: 4/5/2019
+#
+#  create a program that reads a letter of the alphabet from the user. 
+#  If the user enters a, e, i, o or u then your program should display a message
+#  indicating that the entered letter is a vowel. If the user enters y then your program 
+#  should display a message indicating that sometimes y is a vowel, and sometimes y is
+#  a consonant.  Otherwise your program should display a message indicating that the
+#  letter is a consonant.
+def main():
+  day = input("Enter a letter of the alphabet: ") 
+    
+    
+# Start the program.
+main()
