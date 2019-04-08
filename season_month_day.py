c37722c1443282627e666970b15a264c6356304f
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-07 21:51:00 -0400

    Create season_month_day.py

---
+++ season_month_day.py
@@ -0,0 +1,26 @@
+##
+#  season_month_day.py
+#  Author: Mrs. Goldberg
+#  Date: 4/7/2019
+#
+#  The year is divided into four seasons: spring, summer, fall and winter. While the
+#  exact dates that the seasons change vary a little bit from year to year because of the
+#  way that the calendar is constructed, we will use the following dates for this exercise:
+#  Season  First day
+#  Spring  March 20
+#  Summer  June 21
+#  Fall    September 22
+#  Winter  December 21
+#
+#  Create a program that reads a month and day from the user. The user will enter
+#  the name of the month as a string, followed by the day within the month as an
+#  integer. Then your program should display the season associated with the date that
+#  was entered.
+
+def main():
+  month = input("Enter the month: ") 
+  day = int(input("Enter the day: ")    
+    
+    
+# Start the program.
+main()
