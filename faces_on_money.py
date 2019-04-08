619c7bd9b8ed7084a27cd2690821b90fcbcf309a
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-07 21:40:12 -0400

    Create faces_on_money.py

---
+++ faces_on_money.py
@@ -0,0 +1,25 @@
+##
+#  faces_on_money.py
+#  Author: Mrs. Goldberg
+#  Date: 4/7/2019
+#
+#  Write a program that begins by reading the denomination of a banknote from the
+#  user. Then your program should display the name of the individual that appears on the
+#  banknote of the entered amount. An appropriate error message should be displayed
+#  if no such note exists.
+#  Individual         Amount(denomination)
+#  George Washington  $1
+#  Thomas Jefferson   $2
+#  Abraham Lincoln    $5
+#  Alexander Hamilton $10
+#  Andrew Jackson     $20
+#  Ulysses S. Grant   $50
+#  Benjamin Franklin  $100
+
+def main():
+  money = input("Enter the denomination of the banknote: ") 
+    
+    
+    
+# Start the program.
+main()
