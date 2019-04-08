970f9a80c38a9c260277ed1279e1ad01904e93eb
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-07 22:00:56 -0400

    Create assessing_employees.py

---
+++ assessing_employees.py
@@ -0,0 +1,27 @@
+##
+#  assessing_employees.py
+#  Author: Mrs. Goldberg
+#  Date: 4/7/2019
+#
+#  At a particular company, employees are rated at the end of each year. The rating scale
+#  begins at 0.0, with higher values indicating better performance and resulting in larger
+#  raises. The value awarded to an employee is either 0.0, 0.4, or 0.6 or more. Values
+#  between 0.0 and 0.4, and between 0.4 and 0.6 are never used. The meaning associated
+#  with each rating is shown in the following table. The amount of an employee’s raise
+#  is $2400.00 multiplied by their rating.
+#  Rating       Meaning
+#  0.0          Unacceptable performance
+#  0.4          Acceptable performance
+#  0.6 or more  Meritorious performance
+#  Write a program that reads a rating from the user and indicates whether the performance
+#  was unacceptable, acceptable or meritorious. The amount of the employee’s
+#  raise should also be reported. Your program should display an appropriate error
+# message if an invalid rating is entered.
+
+def main():
+  rating = float(input("Enter the denomination of the banknote: ")) 
+    
+    
+    
+# Start the program.
+main()
