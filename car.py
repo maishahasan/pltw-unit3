722817fed20c49bf4b321f94b549109ba74e6d0e
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-04-09 09:09:11 -0400

    Create car.py

---
+++ car.py
@@ -0,0 +1,23 @@
+The Midtown Auto Company produced some models of cars that may be difficult to drive because the car wheels are not exactly round. 
+Car models that exist are from 001 - 999. Cars with model numbers 100-200, 357, 468, 900-999 have been found to have this defect. 
+Create a Defective Car Models application that allows customers to enter the model number of their car to find out if it is defective. 
+If the user enters the model number of a car that is defective, display the message “Your car is defective. Please have it fixed”. 
+If the user enters the model number of a car that is not defective, display the message “Your car is not defective”. 
+If the user enters a model number that is not valid, display the message "Car model does not exist".
+##
+#  car.py
+#  Author: Mrs. Goldberg
+#  Date: 4/9/2019
+#
+#  create a program that reads a letter of the alphabet from the user. 
+#  If the user enters a, e, i, o or u then your program should display a message
+#  indicating that the entered letter is a vowel. If the user enters y then your program 
+#  should display a message indicating that sometimes y is a vowel, and sometimes y is
+#  a consonant.  Otherwise your program should display a message indicating that the
+#  letter is a consonant.
+def main():
+  letter = input("Enter a letter of the alphabet: ") 
+    
+    
+# Start the program.
+main()
