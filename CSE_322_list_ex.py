59351cf08b6f0a63794f56d9e92c313cb28c909a
jgoldbergPCTI <49011050+jgoldbergPCTI@users.noreply.github.com>
2019-05-16 05:36:59 -0400

    Create CSE_322_list_ex.py

---
+++ CSE_322_list_ex.py
@@ -0,0 +1,10 @@
+post_list = []
+post_list.append("aaa")
+post_list.append("bbb")
+post_list.append("ccc")
+print(post_list)
+del post_list[2]
+print(post_list)
+post_list.append("ddd")
+del post_list[0]
+print(post_list)
